bc4c57e13c7efd2cda454b50f2a616319ca1bb98
AEverhub <asusgas097@gmail.com>
2026-08-16 08:12:20 +0800

    Create tststsmm.js

---
+++ tststsmm.js
@@ -0,0 +1,19 @@
+// 檔案路徑：banks/MATH01.js
+window.QUIZ_BANKS = window.QUIZ_BANKS || {};
+
+window.QUIZ_BANKS["MATH01"] = {
+  code: "tststsmm",
+  title: "測試題庫",
+  questions: [
+    {
+      question: "請問 15 + 27 等於多少？",
+      options: ["A. 32", "B. 42", "C. 44", "D. 52"],
+      answer: "B"
+    },
+    {
+      question: "解方程式 2x + 6 = 14，請問 x = ?",
+      options: ["A. 3", "B. 4", "C. 5", "D. 6"],
+      answer: "B"
+    }
+  ]
+};
